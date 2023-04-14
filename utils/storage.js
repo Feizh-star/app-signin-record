b5ba22a0c73dd31ea8a550e3d8f9370d9aa5062b
tujiahao <tujiahao@cnpresky.com>
2023-04-14 19:07:45 +0800

    storage start

--- utils/storage.js
+++ utils/storage.js
@@ -6,6 +6,21 @@ function setStorage(date, key, value) {
 	})
 }
 
+function getStorage(year) {
+	return new Promise((resolve, reject) => {
+		uni.getStorage({
+			key: year,
+			success(data) {
+				resolve(res.data)
+			},
+			fail(err) {
+				reject(err)
+			}
+		})
+	})
+	
+}
+
 function getData() {
 	
 }
